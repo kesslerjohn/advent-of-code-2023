d7f551a5055604cec33d54a8851e5c54927417cc
kesslerjohn <john-kessler@uiowa.edu>
2024-03-21 11:42:01 -0500

    complete day 2-1

--- day2-1.cpp
+++ day2-1.cpp
@@ -9,26 +9,23 @@ int main(){
     int count = 0;
     smatch matches;
     smatch dmatches;
-    vector<regex> exps {regex("\\d+\\ red"), regex("\\d+\\ blue"), regex("\\d+\\ green")};
-    regex red ("\\d+\\ red");
-    regex blue ("\\d+\\ blue");
-    regex green ("\\d+\\ green");
+    vector<regex> exps {regex("\\d+\\ red"), regex("\\d+\\ green"), regex("\\d+\\ blue")};
     regex game ("Game\\ \\d\\d*");
     regex digits ("\\d\\d*");
-    bool possible = true;
     for (string line; getline(cin, line);){
         int thresh = 12;
+        bool possible = true;
         for (regex r : exps){
             string::const_iterator head (line.cbegin());
-            while (regex_search(head, line.cend(), matches, r)){
+            while (regex_search(head, line.cend(), matches, r) & possible){
                 string round = matches[0];
                 if (regex_search(round, dmatches, digits)){
                     string dgt = dmatches[0];
                     possible = possible & (stoi(round) <= thresh);
                 }
-                thresh++;
                 head = matches.suffix().first;
             }
+            thresh++;
         }
         if (possible){
             regex_search(line, matches, game);
@@ -37,7 +34,7 @@ int main(){
             string val = dmatches[0];
             count += stoi(val);
         }
-        cout << line << endl;
     }
+    cout << count << endl;
     return 0;
 }
