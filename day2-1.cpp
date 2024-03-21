a1a4e1f5368c98f597a58b88360f5125b075319e
kesslerjohn <john-kessler@uiowa.edu>
2024-03-21 10:34:13 -0500

    add lots of loops

--- day2-1.cpp
+++ day2-1.cpp
@@ -6,12 +6,48 @@
 using namespace std;
 
 int main(){
+    int count = 0;
     smatch matches;
+    smatch dmatches;
     regex red ("\d\d?\ red");
     regex blue ("\d\d?\ blue");
     regex green ("\d\d?\ green");
-    regex game ("Game\ \d\d?\d?")
+    regex game ("Game\ \d\d?\d?");
+    regex digits ("\d\d?\d?");
+    bool possible = True;
     for (string line; getline(cin, line);){
+        regex_search(line, matches, red); //match reds
+        for (string match; matches;){
+            regex_search(match, dmatches, digits);
+            for (string dgt; dmatches;){
+                if (stoi(dgt) > 12){
+                    possible = False;
+                }
+            }
+        }
+        regex_search(line, matches, green); //match greens
+        for (string match; matches;){
+            regex_search(match, dmatches, digits);
+            for (string dgt; dmatches;){
+                if (stoi(dgt) > 13){
+                    possible = False;
+                }
+            }
+        }
+        regex_search(line, matches, blue); //match blues
+        for (string match; matches;){
+            regex_search(match, dmatches, digits);
+            for (string dgt; dmatches;){
+                if (stoi(dgt) > 14){
+                    possible = False;
+                }
+            }
+        }
+        if (possible){
+            regex_search(line, matches, game);
+            regex_search(matches[0], dmatches, digits);
+            count += stoi(dmatches[0]);
+        }
         cout << line << endl;
     }
     return 0;
