ea8ee56e7586ed2d5a953af0430eb37aa8aa7740
kesslerjohn <john-kessler@uiowa.edu>
2024-03-21 10:21:26 -0500

    make regexes

--- day2-1.cpp
+++ day2-1.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 
 using namespace std;
 
 int main(){
+    smatch matches;
+    regex red ("\d\d?\ red");
+    regex blue ("\d\d?\ blue");
+    regex green ("\d\d?\ green");
+    regex game ("Game\ \d\d?\d?")
     for (string line; getline(cin, line);){
         cout << line << endl;
     }
